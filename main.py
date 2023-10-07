bb3b2d5b237af6d05871d3f9e2d8c47067838492
Silas <145858740+Silas2s@users.noreply.github.com>
2023-10-07 12:34:16 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+# lista = [100,36,145,1]
+# maxi = max(lista)
+# print(maxi)
+
+# lista0 = ['z', 'e', 'r', 't', 'a']
+# lista = [56,566,7878,15,1,0,89,10]
+# organize = sorted(lista0)
+# print(organize)
+
+# for i in range (1,50,2):
+#   print(i)
+
+# for n in range(1,13):
+#   print(n)
+  
+
+# Exercicio 01
+
+# for i in range(2,21,2):
+#   print(i)
+
+# Exercicio 02
+
+# for i in range(5,51,5):
+#   print(i)
+# i = range(5,51,5)
+# print(sum(i))
+
+# Exercicio 03
+
+# x = 5
+# tipo = type(x)
+# print(tipo)
+
+# Exercicio 04
+
+# nome = input('Qual o seu nome?  ')
+# print('Bom dia',nome, ',tudo bem?')
+
+# Exercicio 05
+
+# for i in range(1,10,2):
+#   print(i)
+# i = range(1,10,2,)
+# print(int(sum(i)/len(i)))
+
+
+# palavra = 'Python'
+# for letra in palavra:
+#   print(letra)
+
+# lista = ['maça', 'banana', 'cereja', 'uva']
+# # for i in lista:
+#   # print(i)
+
+#   DICIONÁRIO
+
+# dados = {
+#   'nome': 'Julio',
+#   'idade': 18,
+#   'endereco': 'rua 10'
+# }
+
+# for i1,i2 in dados.items():
+#   print(f'{i1}:{i2}')
+
+# matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# for i in matriz:
+#   for n in i:
+#     print(n)
